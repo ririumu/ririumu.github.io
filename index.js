ec513b508ef06e367b002de31cdf05f74173b907
lilium <33805628+ririumu@users.noreply.github.com>
2023-09-10 20:38:50 +0900

    pr #1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const app: HTMLElement = <div>Have a nice day!</div>;
 const root: HTMLElement = document.getElementById("main");
 ReactDOM.createRoot(root).render(app);
+document.querySelector("body").removeAttribute("hidden");
